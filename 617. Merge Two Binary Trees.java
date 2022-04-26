b9f02bff42595729647af868182fc543bb7516b5
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-04-26 09:52:53 -0400

    Create 617. Merge Two Binary Trees.java

---
+++ 617. Merge Two Binary Trees.java
@@ -0,0 +1,40 @@
+//Recursion 0ms
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1==null) return root2;
+        if (root2==null) return root1;
+        
+        root1.val+=root2.val;
+        root1.left = mergeTrees(root1.left,root2.left);
+        root1.right = mergeTrees(root1.right,root2.right);
+        return root1;
+    }
+}
+
+// stack iterative method
+// 4ms each entry of stack stores data from first & second treenodes
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+       if (root1==null) return root2;
+        Stack < TreeNode[]> stack = new Stack <> ();
+        stack.push(new TreeNode[] {root1, root2});
+        while (!stack.isEmpty()) {
+            TreeNode[] t = stack.pop();
+            if (t[0]==null || t[1] ==null) {
+                continue;
+            }
+            t[0].val +=t[1].val;
+            if (t[0].left == null) {
+                t[0].left = t[1].left;
+            } else {
+                stack.push(new TreeNode[] {t[0].left,t[1].left});
+            }
+            if (t[0].right==null){
+                t[0].right = t[1].right;
+            } else {
+                stack.push(new TreeNode[] {t[0].right,t[1].right});
+            }
+        }
+        return root1;
+    }
+}
