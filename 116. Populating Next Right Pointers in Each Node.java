c234bbfff5374441a19146083f29f36de20e0276
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-04-26 11:14:24 -0400

    Update 116. Populating Next Right Pointers in Each Node.java

--- 116. Populating Next Right Pointers in Each Node.java
+++ 116. Populating Next Right Pointers in Each Node.java
@@ -41,3 +41,18 @@ class Solution {
         return root;
     }
 }
+
+//
+class Solution {
+    public Node connect(Node root) {
+        dfs(root, null);        
+        return root;
+    }
+    
+    private void dfs(Node curr, Node next) {
+        if (curr == null) return;
+        curr.next = next;
+        dfs(curr.left, curr.right);
+        dfs(curr.right, curr.next == null ? null : curr.next.left);
+    }
+}
