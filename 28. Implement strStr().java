ba512a56b927baa3a2453b3e81f2b11a199f11f7
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-04-30 20:17:05 -0400

    Update 28. Implement strStr().java

--- 28. Implement strStr().java
+++ 28. Implement strStr().java
@@ -45,3 +45,31 @@ class Solution {
         return -1;
     }
 }
+
+// improved version to v1 from referring to v2 (i loop stops at m-n+1)
+// 0ms 
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length()<1 || haystack.length()<1 ) return 0;
+        if ( haystack.length()< needle.length()) return -1;
+        int index = -1;
+        for (int i =0; i< haystack.length()-needle.length()+1; i++) {
+            if ((haystack.charAt(i)==needle.charAt(0)) && (index<0)) {
+                index = i;
+                if (needle.length()==1) {
+                    return index; }
+                else {
+                for (int j=1;j<needle.length();j++) {
+                    if (haystack.charAt(i+j)!=needle.charAt(j)) {
+                        index = -1; 
+                        break;}
+                    // System.out.println(index);   
+                }
+                }
+              
+            }
+        }
+        return index;
+    }
+}
