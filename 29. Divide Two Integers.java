6578d5705b13dd36ddcf53e7736b7b1df96df9d9
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-02 00:26:58 -0400

    Update 29. Divide Two Integers.java

--- 29. Divide Two Integers.java
+++ 29. Divide Two Integers.java
@@ -33,6 +33,34 @@ class Solution {
     }
 }
 
+// another simple solution
+// 1ms
+class Solution
+{
+    public int divide(int dividend, int divisor)
+	{
+        if(dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+        
+		int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        int result = 0;
+        int power = 32;
+        long x = Math.abs((long)dividend);
+        long y = Math.abs((long)divisor);
+        
+       while(x >= y)
+		{
+            while((y << power) > x)
+			{
+                power--;
+            }
+            x -= y << power;
+            result += 1 << power;
+        }
+        return result * sign;
+    }
+}
+
 // improved version without using subroutine
 // 1 ms
 class Solution {
@@ -66,5 +94,26 @@ class Solution {
             res += div;
         }
         return sign* (int) res;
+    }
+}
+
+// another fast way
+// 1 ms
+class Solution {
+     public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE; //Cornor case when -2^31 is divided by -1 will give 2^31 which doesnt exist so overflow 
+         
+        boolean negative = dividend < 0 ^ divisor < 0; //Logical XOR will help in deciding if the results is negative only if any one of them is negative
+        
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int quotient = 0, subQuot = 0;
+        
+        while (dividend - divisor >= 0) {
+            for (subQuot = 0; dividend - (divisor << subQuot << 1) >= 0; subQuot++);
+            quotient += 1 << subQuot; //Add to the quotient
+            dividend -= divisor << subQuot; //Substract from dividend to start over with the remaining
+        }
+        return negative ? -quotient : quotient;
     }
 }
