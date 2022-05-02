590b008158fbb3d22ce1276707b4bf74d09bd2d7
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-02 15:29:36 -0400

    Update 31. Next Permutation.java

--- 31. Next Permutation.java
+++ 31. Next Permutation.java
@@ -49,3 +49,33 @@ class Solution {
         }
     }
 }
+
+
+// a better solution
+// idea: search backward. find the next value that is larger than the i-1 th value and swap. Then reverse the orde from i to nums.length-1.
+// 0 ms
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i;
+        for (i=nums.length-1;i>0;--i)
+            if (nums[i]>nums[i-1])
+                break;
+        if (i>0)
+            for (int j=nums.length-1;j>=i;--j)
+                if (nums[j]>nums[i-1]) {
+                    swap(nums,j,i-1);
+                    break;
+                }
+        reverse(nums,i,nums.length-1);
+    }
+    private static void swap(int [] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private static void reverse(int [] nums, int i, int j){
+        while (i<j)
+            swap(nums,i++,j--);
+    }
+}
