91314abbaa1f7be94b7afd4ed3669e06e8d2a72b
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-03 02:00:13 -0400

    Create 33. Search in Rotated Sorted Array.java

---
+++ 33. Search in Rotated Sorted Array.java
@@ -0,0 +1,86 @@
+// my naive solution 
+// 10 ms
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0, j=nums.length-1, firstPt = 0, lastPt=nums.length-1, location=0;
+    if (nums[0]>nums[nums.length-1]) {
+        while (i<j){
+            int k = (i+j)/2;
+            if (nums[k]>nums[i]) {i = k;}
+            else if (nums[k]<nums[i]) {j =k;}
+            else {firstPt=k+1; lastPt=k; break;}         
+        }
+    }
+        System.out.println("firstPt=" + firstPt);
+         if (target<nums[firstPt] || target> nums[lastPt]) { return -1;} 
+         if (target==nums[firstPt] ) {return firstPt;}
+         if (target==nums[lastPt] )  {return  lastPt;}
+        
+     if (nums[0]>nums[nums.length-1]) {
+         
+         if (target<nums[0]) {location= findLoc(nums,firstPt,nums.length-1,target);}
+         else { location= findLoc(nums,0,lastPt,target);}
+         
+     }
+        else {
+            location= findLoc(nums,0,nums.length-1,target);
+        }
+      return location;
+        
+    }
+    private int findLoc(int [] nums,int startPt,int endPt, int target) {
+        int left=startPt, right = endPt;
+        if (target<nums[left] || target> nums[right]) { return -1;} 
+        if (target==nums[left] ) {return left;}
+        if (target==nums[right] )  {return  right;}
+        while (left<right){
+            int mid = (left+right)/2;
+            if (nums[left]==nums[mid] && nums[left]!=target) {return -1;}
+            if (target < nums[mid]) right=mid;
+            else if (target>nums[mid]) left = mid;
+            if (target==nums[mid]) return mid;
+        }
+        return left;
+    }
+}
+
+// clean up version
+// 1ms
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0, j=nums.length-1, firstPt = 0, lastPt=nums.length-1, location=0;
+        // 1) find where the smallest value is in nums
+    if (nums[0]>nums[nums.length-1]) {
+        while (i<j){
+            int k = (i+j)/2;
+            if (nums[k]>nums[i]) {i = k;}
+            else if (nums[k]<nums[i]) {j =k;}
+            else {firstPt=k+1; lastPt=k; break;}         
+        }
+    }
+     // 2) find index of the target value   
+     if (nums[0]>nums[nums.length-1]) {
+         if (target<nums[0]) {location= findLoc(nums,firstPt,nums.length-1,target);}
+         else { location= findLoc(nums,0,lastPt,target);}
+     }
+        else {
+            location= findLoc(nums,0,nums.length-1,target);
+        }
+      return location;
+    }
+    
+    private int findLoc(int [] nums,int startPt,int endPt, int target) {
+        int left=startPt, right = endPt;
+        if (target<nums[left] || target> nums[right]) { return -1;} 
+        if (target==nums[left] ) {return left;}
+        if (target==nums[right] )  {return  right;}
+        while (left<right){
+            int mid = (left+right)/2;
+            if (nums[left]==nums[mid] && nums[left]!=target) {return -1;}
+            if (target < nums[mid]) right=mid;
+            else if (target>nums[mid]) left = mid;
+            if (target==nums[mid]) return mid;
+        }
+        return left;
+    }
+}
