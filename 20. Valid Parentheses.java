432aa184056c12043c8aff736d26299bcf6a88e3
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-02 21:51:13 -0400

    Update 20. Valid Parentheses.java

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -32,3 +32,21 @@
         }
         return stack.isEmpty();
     }
+
+// simple code (5ms)
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack<Character>();
+        for (char ch: s.toCharArray()) {
+            if (ch=='(')
+                stack.push(')');
+            else if (ch=='{')
+                stack.push('}');
+            else if (ch=='[')
+                stack.push(']');
+            else if (stack.isEmpty() || ch!=stack.pop())
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
