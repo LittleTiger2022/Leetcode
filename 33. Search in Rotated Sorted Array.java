064032644509ea15f68b7854027e95e767620f4b
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-03 02:36:23 -0400

    Update 33. Search in Rotated Sorted Array.java

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -84,3 +84,33 @@ class Solution {
         return left;
     }
 }
+
+// simple solution
+// 1ms
+class Solution {
+public int search(int [] A, int target) {
+        int lo=0,hi=A.length -1;
+        if (hi<0) return -1;
+        if (hi==0) {
+            if (A[hi]==target)  return hi;
+            else return -1;
+        }
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if (A[mid]==target) return mid;
+            if (A[lo]<= A[mid]){
+                if (target>=A[lo] && target < A[mid])
+                    hi = mid -1;
+                else 
+                    lo = mid +1;
+            } else {
+                if (target > A[mid] && target <=A[hi])
+                    lo = mid +1;
+                else
+                    hi = mid -1;
+            }
+            
+    }
+    return A[lo] == target ? lo:-1;
+}
+}
