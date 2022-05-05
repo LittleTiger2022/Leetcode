9c11ddf38f91b9821123e5716863bfcc076f36e6
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-05 12:43:02 -0400

    Update 39. Combination Sum.java

--- 39. Combination Sum.java
+++ 39. Combination Sum.java
@@ -33,4 +33,25 @@ class Solution {
     }
 }
 
-//
+// one easier to understand backtracking version
+// 10 ms
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList();
+        backtrack(candidates,0,target,new ArrayList(), res);
+        return res;
+    }
+    private void backtrack(int [] cand, int start,int target,List<Integer> list, List<List<Integer>> res){
+        if (target<0)
+            return;
+        if (target==0) {
+            res.add(new ArrayList(list));
+            return;
+        }
+        for (int i=start;i<cand.length;i++){
+            list.add(cand[i]);
+            backtrack(cand,i,target-cand[i],list,res);
+            list.remove(list.size()-1);
+        }    
+    }
+}
