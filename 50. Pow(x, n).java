a5c1d6d777cada4d7438bff2f7c3cda80678c812
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-07 18:59:53 -0400

    Update 50. Pow(x, n).java

--- 50. Pow(x, n).java
+++ 50. Pow(x, n).java
@@ -20,3 +20,42 @@ class Solution {
     }
     
 }
+
+// avoid overflow
+class Solution {
+    public double myPow(double x, int n) {
+
+        if(n == 0)
+            return 1;
+        
+        if(n == Integer.MIN_VALUE){
+        x = x * x;
+        n = n/2;
+                 }   
+        if(n<0){
+          
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
+}
+      
+// after fix the overflow issue, my original code works
+class Solution {
+    public double myPow(double x, int n) {
+        if (x==0) return 0;
+        if (n==0) return 1;
+
+        if(n == Integer.MIN_VALUE){
+        x = x * x;
+        n = n/2;
+        }   
+
+        if (n<0) {
+            x = 1/x;
+            n = -1*n;
+        }
+        return (n%2==0) ? myPow(x*x,n /2  ): x*myPow(x*x, n/2);
+    }
+}
