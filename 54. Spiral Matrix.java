661f4e22f1f513b544ccb30354396600ebb84e84
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-10 13:15:42 -0400

    Update 54. Spiral Matrix.java

--- 54. Spiral Matrix.java
+++ 54. Spiral Matrix.java
@@ -1,4 +1,5 @@
-// 17 ms, my original code
+// 17 ms, my original code (solution 1)
+// 1ms after removing print out parts and corner cases. (see solution 2)
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int nrow = matrix.length, ncol = matrix[0].length;
@@ -56,6 +57,61 @@ class Solution {
                 bottom--;
             }
                         System.out.println(curID + " switch1 = " + switch1);
+
+            if (switch1==3) {
+                for (int i = bottom; i >=top && curID<totalN; i--) {
+                     list.add(matrix[i][left]);
+                      curID++;
+                }
+                 switch1 =0;
+                  left++;
+            }
+        }
+       return list;  
+    }
+}
+
+// solution 2:
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int nrow = matrix.length, ncol = matrix[0].length;
+        List<Integer> list = new ArrayList<Integer>();
+        
+        int totalN = nrow*ncol;
+        int curRow = 0, curCol = 0, curID = 0;
+        
+        int switch1 = 0; // 0 ->, 1 down, 2 <-, 3 for up.
+        int left=0,right=ncol-1, top=0,bottom = nrow-1;
+        
+        while (curID<totalN) {
+            // left to right
+            if (switch1==0) {
+            for (int i=left; i<=right && curID<totalN; i++) {
+                list.add(matrix[top][i]);
+                curID++;
+            }
+                switch1=1; 
+                top++;
+            }
+            // top to bottom
+            if (switch1==1) {
+                for (int i=top;i<=bottom && curID<totalN;i++) {
+                    list.add(matrix[i][right]);
+                    curID++;
+                }
+                switch1=2;
+                right--;
+            }
+
+            // bottom right to bottom left
+            if (switch1==2) {
+                for (int i = right;i>=left && curID<totalN;i--) {
+                    list.add(matrix[bottom][i]);
+                    curID++;
+                }
+                switch1 =3;
+                bottom--;
+            }
 
             if (switch1==3) {
                 for (int i = bottom; i >=top && curID<totalN; i--) {
