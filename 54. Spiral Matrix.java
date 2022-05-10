fe868709b0242536e6aa27577a9ba9a04632253e
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-10 13:06:21 -0400

    Create 54. Spiral Matrix.java

---
+++ 54. Spiral Matrix.java
@@ -0,0 +1,71 @@
+// 17 ms, my original code
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int nrow = matrix.length, ncol = matrix[0].length;
+        List<Integer> list = new ArrayList<Integer>();
+        if(nrow==1) {
+            for (int i=0; i<ncol; i++)
+            {
+             list.add(matrix[0][i]);
+            }
+            return list;
+        }
+        if(ncol==1) {
+            for (int i=0; i<nrow; i++)
+            {
+             list.add(matrix[i][0]);
+            }
+            return list;
+        }
+        int totalN = nrow*ncol;
+        int curRow = 0, curCol = 0, curID = 0;
+        
+        int switch1 = 0; // 0 ->, 1 down, 2 <-, 3 for up.
+        int left=0,right=ncol-1, top=0,bottom = nrow-1;
+        
+        while (curID<totalN) {
+            System.out.println(curID + " switch1 = " + switch1);
+            // left to right
+            if (switch1==0) {
+            for (int i=left; i<=right && curID<totalN; i++) {
+                list.add(matrix[top][i]);
+                curID++;
+            }
+                switch1=1; 
+                top++;
+            }
+                        System.out.println(curID + " switch1 = " + switch1);
+            // top to bottom
+            if (switch1==1) {
+                for (int i=top;i<=bottom && curID<totalN;i++) {
+                    list.add(matrix[i][right]);
+                    curID++;
+                }
+                switch1=2;
+                right--;
+            }
+                        System.out.println(curID + " switch1 = " + switch1);
+
+            // bottom right to bottom left
+            if (switch1==2) {
+                for (int i = right;i>=left && curID<totalN;i--) {
+                    list.add(matrix[bottom][i]);
+                    curID++;
+                }
+                switch1 =3;
+                bottom--;
+            }
+                        System.out.println(curID + " switch1 = " + switch1);
+
+            if (switch1==3) {
+                for (int i = bottom; i >=top && curID<totalN; i--) {
+                     list.add(matrix[i][left]);
+                      curID++;
+                }
+                 switch1 =0;
+                  left++;
+            }
+        }
+       return list;  
+    }
+}
