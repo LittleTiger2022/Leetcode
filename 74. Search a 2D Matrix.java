8f186e98b8a17220426511c826e0b6bda05d3d86
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-18 00:20:18 -0400

    Update 74. Search a 2D Matrix.java

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -28,3 +28,23 @@ class Solution {
         return false;
     }
 }
+
+// a more concise solution
+// 0ms
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        if (n==0) return false;
+        int m = matrix[0].length;
+        if (m==0) return false;
+        int l=0, r= m*n-1;
+        while (l!=r) {
+            int mid = (l+r-1) >> 1;
+            if (matrix[mid/m][mid%m]<target)
+                l = mid+1;
+            else
+                r = mid;
+        }
+        return matrix[r/m][r%m]==target;
+    }
+}
