d647b4f6a16cf0cf85a7d39b4e1080b4d20b11c4
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-26 00:11:33 -0400

    Update 118. Pascal's Triangle.java

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -33,4 +33,34 @@ class Solution {
         }
         return res;
     }
+    
+    // solution 3: use integer array 
+    class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+  //      ArrayList<Integer> row = new ArrayList<Integer>();
+        int [] pre = new int [numRows];
+        
+        for (int i=0;i<numRows;i++) {
+            int [] row = new int[i+1];
+             row[0] = 1;
+                      //  System.out.println(Arrays.toString(row));
+                       // System.out.println("i = " + i + Arrays.toString(pre));
+
+            if (i>0 ) {
+            for (int j=1;j<=i;j++)
+                row[j] = pre[j-1]+pre[j];
+            }
+           
+            List lst = Arrays.asList(row);
+            List<Integer> intList = new ArrayList<Integer>(i);
+             for (int j=0;j<=i;j++) {
+                pre[j] = row[j];
+                 intList.add(row[j]);
+             }
+            res.add(intList);
+        }
+        return res;
+    }
+}
 }
