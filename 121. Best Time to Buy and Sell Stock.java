ea9ff0eec5b822ba8804b75c32db51561949abe8
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-27 08:08:49 -0400

    Create 121. Best Time to Buy and Sell Stock.java

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,24 @@
+// DP
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length<=1) return 0;
+        int min = prices[0], max = 0;
+        for (int i=1;i<prices.length;i++) {
+            max = Math.max(max,prices[i]-min);
+            min = Math.min(min,prices[i]);
+        }
+        return max;
+    }
+}
+// cumulative sumation
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCur=0,maxSoFar = 0;
+        for (int i=1;i<prices.length;i++) {
+            maxCur = Math.max(0,maxCur+=(prices[i] -prices[i-1] ));
+            maxSoFar = Math.max(maxCur,maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
+//
