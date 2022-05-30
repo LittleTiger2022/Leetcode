1466bbe7fe089505c560a92b2c2a87ecff846259
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-05-30 14:54:07 -0400

    Update 134. Gas Station.java

--- 134. Gas Station.java
+++ 134. Gas Station.java
@@ -33,6 +33,24 @@ class Solution {
         }
         return -1;
     }
+    
+    // solution 2
+    class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total =0, tank = 0, index =0;
+        for (int i=0;i<cost.length;i++) {
+            int cur = gas[i] - cost[i];
+            tank+=cur;
+            
+            if (tank<0) {
+                index = i+1;
+                tank=0;
+            }
+            total +=cur;
+        }
+        return total<0? -1: index;
+    }
+}
 }
 // 1ms 
 class Solution {
