caa54b9608ae339cfdfc8d90811beb988530a7cf
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-06-01 01:00:45 -0400

    Create 160. Intersection of Two Linked Lists.java

---
+++ 160. Intersection of Two Linked Lists.java
@@ -0,0 +1,53 @@
+//
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB==null)
+            return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while (a!=b) {
+            a = a == null? headB:a.next;
+            b = b == null? headA:b.next;
+        }
+        return a;
+    }
+}
+
+// easy to understand version
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+      // find length of each ListNode
+        int lenA =0;
+        ListNode tempA = headA;
+        while (tempA!=null) {
+            lenA++;
+            tempA = tempA.next;
+        }
+        int lenB =0;
+        ListNode tempB = headB;
+        while (tempB!=null) {
+            lenB++;
+            tempB = tempB.next;
+        }
+        
+        int diff = Math.abs(lenA-lenB);
+        // find the diff in lengths and move tempA or tempB to skipe the first extra nodes.
+        tempA = headA;
+        tempB = headB;
+        
+        if (lenA > lenB) {
+            while (diff-- >0) 
+                tempA = tempA.next;
+        } else {
+            while (diff-- >0)
+                tempB = tempB.next;
+        }
+        // with A and B have equal # of nodes to the end, check for equality for intersecttion 
+        while (tempA!=tempB) {
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+        return tempA;
+    }
+}
