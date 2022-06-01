7dddec35b7a07a5429df9fbe3626533cf0ff6160
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-06-01 01:13:12 -0400

    Update 160. Intersection of Two Linked Lists.java

--- 160. Intersection of Two Linked Lists.java
+++ 160. Intersection of Two Linked Lists.java
@@ -51,3 +51,21 @@ public class Solution {
         return tempA;
     }
 }
+
+// use HashMap (6ms)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        while (headA!=null) {
+            visited.add(headA);
+            headA = headA.next;
+        }
+        
+        while (headB!=null) {
+            if (visited.contains(headB))
+                return headB;
+            headB= headB.next;
+        }
+        return null;
+    }
+}
