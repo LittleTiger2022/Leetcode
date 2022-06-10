1795854f50b21e5dc15cffa7d830911b2d0a79ed
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-06-10 12:08:18 -0400

    Create 238. Product of Array Except Self.java

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,16 @@
+// O(N) loop from left to right in muliplication except for using the i-th nums. Then loop from the righ to left in multiplication (also excpt from using the ith num)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int [] res = new int [nums.length];
+        res[0] = 1;
+        for (int i=1; i<nums.length;i++) {
+            res[i] = res[i-1]*nums[i-1];
+        }
+        int right = 1;
+        for (int i=nums.length-1;i>=0;i--) {
+            res[i] *= right;
+            right*=nums[i];
+        }
+        return res;
+    }
+}
