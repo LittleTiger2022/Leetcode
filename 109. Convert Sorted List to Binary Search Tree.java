7061523ecf093f6ba008385d9f947661c264f758
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-06-23 06:04:49 -0400

    Create 109. Convert Sorted List to Binary Search Tree.java

---
+++ 109. Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,28 @@
+// s1 (best sol)
+class Solution {
+    private ListNode node;
+    public TreeNode sortedListToBST(ListNode head) {
+         if (head == null)
+             return null;
+        int size = 0;
+        ListNode runner = head;
+        node = head;
+        while (runner !=null) {
+            runner = runner.next;
+            size++;
+        }
+        return inorderHelper(0,size-1);
+    }
+    public TreeNode inorderHelper(int start, int end) {
+        if (start>end)
+            return null;
+        int mid = (start + end)/2;
+        TreeNode left = inorderHelper(start,mid-1);
+        TreeNode treenode = new TreeNode(node.val);
+        treenode.left = left;
+        node = node.next;
+        TreeNode right = inorderHelper(mid+1,end);
+        treenode.right = right;
+        return treenode;
+    }
+}
