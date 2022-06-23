7c646e33a9186f9de750536167e05bc9f9efc7d6
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-06-22 22:52:49 -0400

    Update 100. Same Tree.java

--- 100. Same Tree.java
+++ 100. Same Tree.java
@@ -1,5 +1,5 @@
 // my naive solution
-// O(N) recursive 0ms, 100% & 5.18% in memory
+// O(N) recursive 0ms, 100% & O(nlogn) 5.18% in memory
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         return isSame(p,q);
@@ -39,4 +39,49 @@ class Solution {
             return false;
         return isSame(p.left,q.left) & isSame(p.right,q.right);
     }
+}
+
+// offical 2nd solution: Iteration using ArrayDeque
+// O(N) , space: O(log(N))
+class Solution {
+  public boolean check(TreeNode p, TreeNode q) {
+    // p and q are null
+    if (p == null && q == null) return true;
+    // one of p and q is null
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return true;
+  }
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (!check(p, q)) return false;
+
+    // init deques
+    ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+    ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+    deqP.addLast(p);
+    deqQ.addLast(q);
+
+    while (!deqP.isEmpty()) {
+      p = deqP.removeFirst();
+      q = deqQ.removeFirst();
+
+      if (!check(p, q)) return false;
+      if (p != null) {
+        // in Java nulls are not allowed in Deque
+        if (!check(p.left, q.left)) return false;
+        if (p.left != null) {
+          deqP.addLast(p.left);
+          deqQ.addLast(q.left);
+        }
+        if (!check(p.right, q.right)) return false;
+        if (p.right != null) {
+          deqP.addLast(p.right);
+          deqQ.addLast(q.right);
+        }
+      }
+    }
+    return true;
+  }
 }
