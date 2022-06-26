41666b09050c15d5bc63bbed2045fd21d8603bcf
LittleTiger2022 <103449088+LittleTiger2022@users.noreply.github.com>
2022-06-26 16:22:07 -0400

    Create 129. Sum Root to Leaf Numbers.java

---
+++ 129. Sum Root to Leaf Numbers.java
@@ -0,0 +1,13 @@
+// DFS
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return sum(root,0);
+    }
+    private int sum(TreeNode n, int s) {
+        if (n==null)
+            return 0;
+        if (n.right==null && n.left==null)
+            return s*10+n.val;
+        return sum(n.left,s*10+n.val) + sum(n.right,s*10+n.val);
+    }
+}
